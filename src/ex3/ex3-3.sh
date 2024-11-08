cf3451520c5658bf59c8e62e683f9e4279e5cffa
nayoung507 <164850484+nayoung507@users.noreply.github.com>
2024-11-08 15:51:47 +0900

    Update ex3-3.sh

--- src/ex3/ex3-3.sh
+++ src/ex3/ex3-3.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-read -p "Enter your height (cm): " height
-read -p "Enter your weight (kg): " weight
+read -p "신장(cm)을 입력하세요: " height
+read -p "몸무게(kg)를 입력하세요: " weight
 
 height_m=$(echo "$height / 100" | bc -l)
 
 bmi=$(echo "scale=2; $weight / ($height_m * $height_m)" | bc)
 
-is_underweight=$(echo "$bmi < 18.5" | bc)
-is_normal=$(echo "$bmi >= 18.5 && $bmi < 25" | bc)
-is_overweight=$(echo "$bmi >= 25" | bc)
+underweight=$(echo "$bmi < 18.5" | bc)
+normal=$(echo "$bmi >= 18.5 && $bmi < 25" | bc)
+overweight=$(echo "$bmi >= 25" | bc)
 
-if [ "$is_underweight" -eq 1 ]; then
-    echo "Underweight"
-elif [ "$is_normal" -eq 1 ]; then
-    echo "Normal weight"
-elif [ "$is_overweight" -eq 1 ]; then
-    echo "Overweight"
+if [ "$underweight" -eq 1 ]; then
+    echo "저체중입니다. BMI = $bmi"
+elif [ "$normal" -eq 1 ]; then
+    echo "정상체중입니다. BMI = $bmi"
+elif [ "$overweight" -eq 1 ]; then
+    echo "비만입니다. BMI = $bmi"
 else
-    echo "Error in BMI calculation."
+    echo "BMI 계산에 오류가 발생했습니다."
 fi
