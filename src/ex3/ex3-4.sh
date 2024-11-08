7a52f68d939c3cbd98347e4b92dcb4e1970005f7
nayoung507 <164850484+nayoung507@users.noreply.github.com>
2024-11-08 19:45:53 +0900

    Update ex3-4.sh

--- src/ex3/ex3-4.sh
+++ src/ex3/ex3-4.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-answer_check() {
-    case "$1" in
-        [yY] | [yY][eE][sS])
-            echo "좋아요!"
-            ;;
-        [nN] | [nN][oO])
-            echo "안타깝네요."
-            ;;
-        *)
-            echo "yes 또는 no로 입력해 주세요."
-            ;;
-    esac
-}
-
 read -p "리눅스가 재미있나요? (yes / no): " answer
 
-answer_check "$answer"
+case $answer in
+    [yY] | [yY][eE][sS])
+        echo "좋아요!"
+        ;;
+    [nN] | [nN][oO])
+        echo "안타깝네요."
+        ;;
+    *)
+        echo "yes 또는 no로 입력해 주세요."
+        ;;
+esac
